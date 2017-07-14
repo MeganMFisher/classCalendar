15f3724b9b2edb348a2c96ae086a545ebf20f3e6
MeganMFisher <meganmichelle318@gmail.com>
2017-07-14 15:44:50 -0600

    correct calendar

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,4 +1,27 @@
-angular.module('app').controller('mainCtrl', function($scope, mainSrv){
+angular.module('app').controller('mainCtrl', function($scope, $compile, uiCalendarConfig, mainSrv){
 
  $scope.eventSources = [];
+
+//    $scope.showModal = false
+
+//   $scope.events = []
+
+  $scope.uiConfig = {
+    calendar: {
+      height: 800,
+      defaultView: "agendaWeek",
+      editable: true,
+      selectable: true,
+      header: {
+        left: 'today prev,next',
+        center: 'title',
+        right: 'month, agendaWeek agendaDay'
+      }
+    //   eventClick: $scope.alertEventOnClick,
+    //   eventDrop: $scope.alertOnDrop,
+    //   eventResize: $scope.alertOnResize,
+    //   eventRender: $scope.eventRender
+
+    }
+  };
 })
