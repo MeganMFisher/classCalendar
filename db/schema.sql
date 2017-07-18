4c5a28ee7edfd0818bd193c3740be8fc7678ab01
MeganMFisher <meganmichelle318@gmail.com>
2017-07-18 13:32:58 -0600

    updates

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE events(
  title varchar,
  description varchar,
- start_time varchar,
- end_time varchar
+ start_time date,
+ end_time date
 )
 
 INSERT INTO events(title, description, start_time, end_time) values ('movie marathon', 'awesomeness', '10-11-17', '10-13-17')
