153f2f0e5bab710b63c33915948a7ef9c146ede8
MeganMFisher <meganmichelle318@gmail.com>
2017-08-07 10:19:37 -0600

    fixing stuff

--- public/dist/all.js
+++ public/dist/all.js
@@ -7,18 +7,26 @@ angular.module('app', ['ui.calendar', 'ui.router']).config(function ($stateProvi
     $stateProvider.state('home', {
         url: '/',
         templateUrl: './views/home.html'
-    }).state('calendar', {
-        url: '/calendar',
-        templateUrl: './views/calendar.html'
-    }).state('goals', {
-        url: '/goals',
-        templateUrl: './views/goals.html',
-        controller: 'goalCtrl'
-    }).state('todo', {
-        url: '/todo',
-        templateUrl: './views/todo.html',
-        controller: 'todoCtrl'
-    }).state('resources', {
+    })
+
+    // .state('calendar', {
+    //     url: '/calendar',
+    //     templateUrl: './views/calendar.html'
+    // })
+
+    // .state('goals',{
+    //     url: '/goals',
+    //     templateUrl: './views/goals.html',
+    //     controller: 'goalCtrl'
+    // })
+
+    // .state('todo', {
+    //     url: '/todo',   
+    //     templateUrl: './views/todo.html',
+    //     controller: 'todoCtrl'
+    // })
+
+    .state('resources', {
         url: '/resources',
         templateUrl: './views/resources.html'
     }).state('links', {
